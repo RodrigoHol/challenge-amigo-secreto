4d7738aaeac51bc93ea5c0673d26cf99b95ed4d6
rodri <rodrigokum091@gmail.com>
2025-01-27 19:51:13 -0600

    crear funcion actualizarLista

--- app.js
+++ app.js
@@ -6,10 +6,23 @@ function agregarAmigo() {
   let amigo = document.getElementById("amigo");
 
   if (amigo.value) {
-    amigos.push(amigo.value);
+    amigos.push(amigo.value.trim());
     amigo.value = "";
+    actualizarLista();
     console.log(amigos);
   } else {
     alert("Por favor, ingrese un nombre");
   }
+}
+
+function actualizarLista() {
+  let listaAmigos = document.getElementById("listaAmigos");
+
+  listaAmigos.innerHTML = "";
+
+  amigos.forEach((amigo) => {
+    const li = document.createElement("li");
+    li.textContent = amigo;
+    listaAmigos.appendChild(li);
+  });
 }
